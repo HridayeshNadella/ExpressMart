7b87c57e280ab1746f21d695d4a2acdb9cc18a5a
Geek1234 <nadellahridayesh@gmail.com>
2024-11-09 02:21:16 +0530

    added men/women/kids category and login page

--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import './CSS/LoginSignup.css';
+import loginimage from '../assets/login_image.png';
 const LoginSignup = () => {
   return (
-    <div>
-      
+    <div className='loginsignup'>
+      <div className="loginsignup-wrapper">
+      <img src={loginimage} alt="Signup Visual" className="loginsignup-image" />
+      <div className="loginsignup-container">
+        <h1>Signup</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your Name:'/>
+          <input type='email' placeholder='Email Address:'/>
+          <input type='password' placeholder='Password:'/>
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">Already have an account? <span>Login Here</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name='' id=''/>
+          <p>By Continuing,I agrree to the terms of  use & privacy</p>
+        </div>
+      </div>
+      </div>
     </div>
   )
 }
